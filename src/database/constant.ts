06623f5266199adcbf359a0ce8ef1fe6101c22ef
unknown <bennyizkandar@gmail.com>
2022-03-08 11:09:30 +0800

    update database

---
+++ src/database/constant.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export const SEQUELIZE = 'SEQUELIZE';
+export const DEVELOPMENT = 'development';
+export const TEST = 'test';
+export const PRODUCTION = 'production';
+export const USER_REPOSITORY = 'USER_REPOSITORY';
+export const BOOK_REPOSITORY = 'BOOK_REPOSITORY';
+export const RENT_REPOSITORY = 'RENT_REPOSITORY';
+export const AUTH_REPOSITORY = 'AUTH_REPOSITORY';
