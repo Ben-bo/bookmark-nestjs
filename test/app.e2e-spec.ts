1d24169340fdd32e27101688e8c6f6ca33717689
unknown <bennyizkandar@gmail.com>
2022-03-10 09:45:09 +0800

    update e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto/auth.dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -11,13 +13,117 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
+    pactum.request.setBaseUrl('http://localhost:3333/');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('pass');
-  it.todo('pass2');
-  it.todo('pass3');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'beni1@gmail.com',
+      password: '123456',
+    };
+    describe('Signin', () => {
+      let accessToken: string;
+      it('it should throw an error 400 if email is empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should throw an error 400 if password is empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should throw an error 400 if no input', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({})
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should SignIn', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect()
+          .stores('userAt', 'token');
+      });
+    });
+    describe('Signup', () => {
+      const dto: AuthDto = {
+        email: 'beni4@gmail.com',
+        password: '123456',
+      };
+      it('it should throw an error 400 if password is empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should throw an error 400 if email is empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should throw an error 400 if no input', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400)
+          .inspect();
+      });
+      it('it should signup', () => {
+        return pactum
+          .spec()
+          .get('auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+      });
+    });
+  });
+  describe('User', () => {
+    describe('Get Me', () => {
+      it('it should get current user', () => {
+        return pactum.spec().post('user/me').expectStatus(200);
+      });
+    });
+    describe('Edit User', () => {});
+  });
+  describe('Bookmark', () => {
+    describe('Create Bookmark', () => {});
+    describe('Get All Bookmark', () => {});
+    describe('Get Bookmark By Id', () => {});
+    describe('Update Bookmark', () => {});
+    describe('Delete Bookmark', () => {});
+  });
 });
